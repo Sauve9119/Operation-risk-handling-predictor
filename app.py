415ddc958bd5b22b6be0f6a3aef4031f552f6549
Rachit Gupta <guptarachit0903@gmail.com>
2026-03-23 21:50:43 +0530

    Implement data loading and heatmap plotting
    Added data processing and correlation heatmap visualization.

--- app.py
+++ app.py
@@ -1,6 +1,31 @@
 import streamlit as st
 import numpy as np
 import joblib
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("/content/How Ready Are You for Real-World Job Situations_  (Responses) - Form Responses 1.csv")
+df = df.iloc[:,2:]
+for col in df.columns:
+    unique_vals = df[col].unique()
+    mapping = {val: i+1 for i, val in enumerate(unique_vals)}
+    df[col] = df[col].map(mapping)
+
+# correlation heatmap
+corr_matrix = df.corr()
+
+ # Plot the heatmap
+plt.figure(figsize=(8, 6))
+sns.heatmap(corr_matrix, 
+            annot=True,      # Show numerical values
+            cmap='coolwarm', # Color scheme
+            vmin=-1, vmax=1, # Scale for correlation
+            fmt=".2f",       # Format to 2 decimal places
+            linewidths=0.5)  # Add lines between cells
+
+plt.title('Correlation Heatmap')
+plt.show()
 
 # ---------------- LOAD MODEL ----------------
 model = joblib.load("model.pkl")
