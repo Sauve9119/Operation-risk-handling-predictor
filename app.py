a4b014746c95e7ab90ef67eeddaf379d45ff87e1
Rachit Gupta <guptarachit0903@gmail.com>
2026-03-27 22:42:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -172,7 +172,7 @@ elif page == "Model Training":
     # -------- SPLIT --------
     from sklearn.model_selection import train_test_split
     X_train, X_test, y_train, y_test = train_test_split(
-        X_scaled, y, test_size=0.3, random_state=42
+        X, y, test_size=0.3, random_state=42
     )
 
     # -------- SCALE --------
